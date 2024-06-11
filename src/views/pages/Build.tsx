80f2b6e531310cc814661f718fa96e3a42f0b342
Shivam Yadav <codecshivam@gmail.com>
2024-06-11 13:26:00 +0530

    Fix

--- src/views/pages/Build.tsx
+++ src/views/pages/Build.tsx
@@ -2,14 +2,14 @@
 
 const Build = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-green-50 to-[#FBFCF7]">
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-r from-[#FBFCF7] to-[#D7E6C5]">
       <div className="text-center p-8 bg-white shadow-lg rounded-lg">
         <h1 className="text-4xl font-bold text-gray-800 mb-4">🚧 Under Construction 🚧</h1>
         <p className="text-lg text-gray-600 mb-6">
           We're currently working on this page.
         </p>
         <button
-          className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-lg hover:bg-blue-600 transition duration-300"
+          className="px-6 py-3 bg-[#D7E6C5] text-black font-semibold rounded-lg hover:bg-[#FBFCF7] transition duration-300"
           onClick={() => window.location.href = "/"}
         >
           Go Back Home
--- src/views/partials/Sidebar/Sidebar.tsx
+++ src/views/partials/Sidebar/Sidebar.tsx
@@ -3,7 +3,7 @@ import SideNavBar from "./SideNavBar";
 
 const SideBar = () => {
   return (
-    <section className="hidden lg:flex flex-col  w-80 bg-transparentBG relative h-full px-2 py-8 rounded-2xl">
+    <section className="hidden lg:flex flex-col  w-80 bg-transparentBG relative h-full pl-2  py-8 rounded-2xl">
       <div className="h-12">
       </div>
       <SideNavBar />
