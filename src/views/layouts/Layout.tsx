4e83cd1c01b7dccc531db54d1ec220080476eda3
Shivam Yadav <codecshivam@gmail.com>
2024-06-30 14:36:31 +0530

    sidebar fixes

--- src/views/layouts/Layout.tsx
+++ src/views/layouts/Layout.tsx
@@ -15,14 +15,14 @@ const Layout = () => {
     };
 
     return (
-        <div className="lg:h-screen lg:overflow-y-hidden">
+        <div className="relative  lg:h-screen lg:overflow-y-hidden">
             <Header />
-            <div className="relative  flex " >
+            <div className="flex">
                 <div className=" h-screen bg-[#FBFCF7] w-fit">
                     <SideBar />
                 </div>
-                <div className="w-full  scroll-smooth  lg:overflow-scroll">
-                    <div className="relative flex z-10 items-center my-auto gap-4 lg:hidden">
+                <div className="w-full  scroll-smooth   lg:overflow-scroll">
+                    <div className=" flex z-10 items-center my-auto gap-4 lg:hidden">
                         <div
                             onClick={handleOpen}
                             className={cn(
@@ -32,7 +32,7 @@ const Layout = () => {
                         ></div>
                         <div
                             className={cn(
-                                "fixed top-0 bottom-0 left-0 z-30 w-2/3 py-8 -translate-x-full transition-transform duration-150 bg-gradientBG",
+                                "fixed top-0 bottom-0 left-0 z-30 w-2/3 py-8 -translate-x-full transition-transform duration-150 bg-[#FBFCF7]",
                                 openNav && "translate-x-0"
                             )}
                         >
@@ -44,7 +44,7 @@ const Layout = () => {
                             </div>
                             <SideNavBar handleOpen={handleOpen} />
                         </div>
-                        <button className="mt-7 ml-2" onClick={handleOpen}>
+                        <button className="mt-7 ml-2 absolute top-0" onClick={handleOpen}>
                             <Menu className="w-8 h-8 text-black" />
                         </button>
 
