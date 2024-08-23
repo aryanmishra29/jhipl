441f10f011e898085dc19e9c6ab5ecb14bcf35d4
Aryan Mishra <aryanmishracoc@gmail.com>
2024-08-23 11:53:52 +0530

    changes

--- src/views/pages/Login.tsx
+++ src/views/pages/Login.tsx
@@ -76,41 +76,43 @@ function Login() {
     };
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-[#FBFCF7]">
-            <div className="bg-white shadow-lg rounded-lg p-8 max-w-sm w-full">
-                <h2 className="text-2xl font-semibold text-gray-700 mb-6 text-center">Login</h2>
-                <form onSubmit={handleLogin}>
-                    <div className="mb-4">
-                        <label className="block text-gray-600 mb-2">Email</label>
-                        <input
-                            type="email"
-                            placeholder="Email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black"
-                            required
-                        />
-                    </div>
-                    <div className="mb-6">
-                        <label className="block text-gray-600 mb-2">Password</label>
-                        <input
-                            type="password"
-                            placeholder="Password"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black"
-                            required
-                        />
-                    </div>
-                    <button
-                        type="submit"
-                        className="w-full bg-black text-white py-2 rounded-md hover:bg-gray-800 transition duration-300"
-                    >
-                        Login
-                    </button>
-                </form>
+      <div className="min-h-screen flex items-center justify-center bg-[#FBFCF7]">
+        <div className="bg-white shadow-lg rounded-lg p-8 max-w-sm w-full">
+          <h2 className="text-2xl font-semibold text-gray-700 mb-6 text-center">
+            Login
+          </h2>
+          <form onSubmit={handleLogin}>
+            <div className="mb-4">
+              <label className="block text-gray-600 mb-2">Email</label>
+              <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className=" bg-white  text-black w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black"
+                required
+              />
             </div>
+            <div className="mb-6">
+              <label className="block text-gray-600 mb-2">Password</label>
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full bg-white  text-black px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-black"
+                required
+              />
+            </div>
+            <button
+              type="submit"
+              className="w-full bg-black text-white py-2 rounded-md hover:bg-gray-800 transition duration-300"
+            >
+              Login
+            </button>
+          </form>
         </div>
+      </div>
     );
 }
 
